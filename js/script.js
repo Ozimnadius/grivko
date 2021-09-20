a5096cdfb875ed0b3567038e5af4a33c947712e6
Mikle Seregin <mikhail-seregin@yandex.ru>
2021-09-20 17:51:17 +0300

    day 5

--- js/script.js
+++ js/script.js
@@ -82,7 +82,77 @@ window.addEventListener("load", function (){
 
 });
 //choose.js
-window.addEventListener("load", function (){
+window.addEventListener("load", function () {
     console.log('CHOOSE.JS');
+
+    class Check {
+        constructor() {
+            this.nav = $(".choose__nav");
+            this.inputus = $(".choose-slide__input");
+
+            this.inputus.on("change", () => {
+                this.activateNav();
+            });
+        }
+
+        check(slide) {
+
+            if (!slide.find('.choose-slide__input:checked').length > 0) {
+                this.deactivateNav();
+            } else {
+                this.activateNav();
+            }
+        }
+
+        activateNav() {
+            this.nav.removeClass("disabled");
+        }
+
+        deactivateNav() {
+            this.nav.addClass("disabled");
+        }
+    }
+
+    let test = new Check();
+
+    const chooseSwiper = new Swiper('.choose__swiper', {
+        // initialSlide: 5,
+        spaceBetween: 10,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        allowTouchMove: false,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.choose__next',
+            prevEl: '.choose__prev',
+        },
+        // If we need pagination
+        pagination: {
+            el: '.choose__pag',
+        },
+        // onAny(eventName,...args){
+        //     console.log(eventName);
+        // },
+        on: {
+            init: function () {
+                let $slide = $(this.slides[this.activeIndex]),
+                    $checked = $slide.find('.choose-slide__input:checked');
+                test.check($slide);
+
+            },
+            slideChange: function () {
+                let $slide = $(this.slides[this.activeIndex]),
+                    $checked = $slide.find('.choose-slide__input:checked');
+
+                test.check($slide);
+
+
+            }
+        }
+    });
+
+
 });
 //# sourceMappingURL=script.js.map
