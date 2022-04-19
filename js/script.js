055714d4eeb3dd77f7ffe2865e0e47a9a156097f
Mikle Seregin <mikhail-seregin@yandex.ru>
2022-04-19 15:42:47 +0300

    grivko(2.0) - Женщинам / Пошив на заказ / Технологии пошива (/women/tailoring/technology.html)

--- js/script.js
+++ js/script.js
@@ -1025,28 +1025,30 @@ window.addEventListener('load', function (){
 });
 window.addEventListener('load', function (){
 
-    let type = 'absolute';
-    if (window.matchMedia('(max-width: 767.98px)').matches){
-        type = 'default';
+    if(document.querySelector('.fabrics-tabs')) {
+        let type = 'absolute';
+        if (window.matchMedia('(max-width: 767.98px)').matches) {
+            type = 'default';
+        }
+
+        let tabs = new OzimnadTabs({
+            selector: '.fabrics-tabs',
+            btnSelector: '.fabrics-tabs__btn',
+            tabSelector: '.fabrics-tabs__tab',
+            type: type
+        });
+
+
+        let $select = $('.fabrics-tabs__select');
+
+        $select.styler();
+
+        $select.on('change', function () {
+            tabs.deactivate();
+            tabs.activeIndex = this.value;
+            tabs.activate();
+        });
     }
-
-    let tabs = new OzimnadTabs({
-        selector: '.fabrics-tabs',
-        btnSelector: '.fabrics-tabs__btn',
-        tabSelector: '.fabrics-tabs__tab',
-        type: type
-    });
-
-
-    let $select = $('.fabrics-tabs__select');
-
-    $select.styler();
-
-    $select.on('change', function (){
-        tabs.deactivate();
-        tabs.activeIndex = this.value;
-        tabs.activate();
-    });
 
 });
 //# sourceMappingURL=script.js.map
--- src/components/fabrics/tabs/script.js
+++ src/components/fabrics/tabs/script.js
@@ -1,26 +1,28 @@
 window.addEventListener('load', function (){
 
-    let type = 'absolute';
-    if (window.matchMedia('(max-width: 767.98px)').matches){
-        type = 'default';
+    if(document.querySelector('.fabrics-tabs')) {
+        let type = 'absolute';
+        if (window.matchMedia('(max-width: 767.98px)').matches) {
+            type = 'default';
+        }
+
+        let tabs = new OzimnadTabs({
+            selector: '.fabrics-tabs',
+            btnSelector: '.fabrics-tabs__btn',
+            tabSelector: '.fabrics-tabs__tab',
+            type: type
+        });
+
+
+        let $select = $('.fabrics-tabs__select');
+
+        $select.styler();
+
+        $select.on('change', function () {
+            tabs.deactivate();
+            tabs.activeIndex = this.value;
+            tabs.activate();
+        });
     }
-
-    let tabs = new OzimnadTabs({
-        selector: '.fabrics-tabs',
-        btnSelector: '.fabrics-tabs__btn',
-        tabSelector: '.fabrics-tabs__tab',
-        type: type
-    });
-
-
-    let $select = $('.fabrics-tabs__select');
-
-    $select.styler();
-
-    $select.on('change', function (){
-        tabs.deactivate();
-        tabs.activeIndex = this.value;
-        tabs.activate();
-    });
 
 });
